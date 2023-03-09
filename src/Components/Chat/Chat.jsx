3a67402ad9a8adc70f0cfeda4ccdce0275a5dee0
danielfranchi <danielfranchi3@gmail.com>
2023-03-09 09:29:53 -0300

    commit

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -21,13 +21,12 @@ import {
 } from "@chakra-ui/react";
 import "./Chat.scss";
 import { addDoc, collection, deleteDoc, doc, FieldValue, serverTimestamp, Timestamp } from "firebase/firestore";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
-
 
 const Chat = ({messages,dataUser}) => {
   const [valueInput,setValueInput]=useState('')
- 
+  const navigate =useNavigate()
  const messagesCollection = collection(db,'messages')
  const user = auth.currentUser
 const handleChange = (e)=>{
@@ -51,17 +50,20 @@ const deleteMessage = async (id)=>{
 
 const userLogOut = async ()=>{
   await signOut(auth)
+  navigate('/')
+
 }
 
   return (
     <div >
-<Box bg='#38598b' w='800px' p={4} color='white'
+<Box bg='#38598b' style={{display:'flex',justifyContent:'space-between'}} w='800px' p={4} color='white'
         maxW={{ base: '350px', sm: '600px' }}
 >
   
   <Heading> Chat</Heading>
+  <Heading><Button bg='white'style={{color:'black'}} onClick={()=>userLogOut()}>Logout</Button></Heading>
 </Box>
-<Box bg='#e7eaf6' w='800px' h='400px' p={4} color='white'
+<Box className="boxOne" bg='#e7eaf6' w='800px' h='400px' p={4} color='white'
 maxW={{ base: '350px', sm: '600px' }}>
 
   {messages.sort((a,b)=>a.createdAt - b.createdAt).map((mes)=>(
@@ -72,14 +74,13 @@ maxW={{ base: '350px', sm: '600px' }}>
           <WrapItem>
           <Avatar
       size='xs'
-      name='D'
-      src='https://bit.ly/tioluwani-kolawole'
+      bg='teal.500'
     />          <span style={{fontSize:'10px',padding:'5px',color:'black'}}> {user?user.displayName:null}</span> 
 
           </WrapItem>
           
         </Wrap>
-          <Box  style={{borderRadius:'10px'}} bg='#a2a8d3' w='100%' p={2} m='2'  color='white'
+          <Box  style={{borderRadius:'10px'}} bg='#a2a8d3' w='150px' p={2} m='2'  color='white'
         maxW={{ base: '120px', sm: '300px' }}
         > 
        <p style={{fontSize:'17px'}} onClick={()=>deleteMessage(mes.id)}>{mes.text}</p>
@@ -87,10 +88,23 @@ maxW={{ base: '350px', sm: '600px' }}>
         </div>
         
       
-      :
-            <Box className="box"  bg=' #113f67'>
+      :  <div className="box">
+                    <Wrap style={{display:'flex',alignItems:'center'}}>
+          <WrapItem>
+          <Avatar
+      size='xs'
+      bg='red.500'
+      src='https://bit.ly/tioluwani-kolawole'
+    />          <span style={{fontSize:'10px',padding:'5px',color:'black'}}> User </span> 
+
+          </WrapItem>
+          
+        </Wrap>
+        <Box bg=' #113f67' style={{borderRadius:'10px'}}>
             <p style={{padding:'10px'}}> {mes.text}</p>
           </Box>
+      </div>
+            
           
           }
 
@@ -104,103 +118,9 @@ maxW={{ base: '350px', sm: '600px' }}>
         maxW={{ base: '350px', sm: '600px' }}
 >
    <form action="" onSubmit={handleSubmit}>
-      <Input  value={valueInput} onChange={handleChange }  size='lg' variant='flushed' placeholder="message"/>
+      <Input style={{color:'white'}}  value={valueInput} onChange={handleChange }  size='lg' variant='flushed' placeholder="message"/>
    </form>
 </Box>
-
-
-
-{/*        <Center>
-      <Grid
-  templateAreas={`"header header"
-                  "main main"
-                  "footer footer"`}
-  gridTemplateRows={'50px 1fr 30px'}
-  gridTemplateColumns={'150px 1fr'}
-  h='500px'
-  w='500px'
-  gap='0'
-  
-  color='blackAlpha.700'
-  fontWeight='bold'
->
-<GridItem  bg='#0092ca'  area={'header'}
-  
-  >
-    <Flex>
-      <Box p='2'>
-     </Box>
-      <Spacer/>
-      <Box p='1'>
-        <Link to={'/SignIn'}><Button bg='#222831' style={{color:'white'}}  onClick={()=>userLogOut()}>LogOut</Button> </Link>  
-      </Box>
-     
-    
-    </Flex>
-
-  </GridItem>
-
-  <GridItem  className="grid-item" bg='#393e46' area={'main'}
-  >       
-  {messages.sort((a,b)=>a.createdAt - b.createdAt).map((item)=>(
-    <div key={item.id}>
-      <div>
-        
-        <Wrap >
-               {item.uid === user.uid?
-               
-               <WrapItem style={{display:'flex',flexDirection:'column',marginLeft:'380px',marginTop:'50px',backgroundColor:'#eeeeee',borderRadius:'5px'}}>
-                <div style={{display:'flex'}}>
-                  <div>
-                    <Avatar size='xs'/>
-                  </div>
-                  <div>
-                    <span style={{fontSize:'12px'}}> {user?user.displayName:'null'}</span>
-                    <span style={{fontSize:'12px'}}> {user?user.createdAt:'null'}</span>
-                  </div>
-                </div>
-                      <p style={{width:'200px',margin:'5px'}} onClick={()=>deleteMessage(item.id)}>{item.text} </p> 
-               </WrapItem>
-          :
-          <WrapItem style={{backgroundColor:'#eeeeee',display:'flex',flexDirection:'column',marginLeft:'20px',borderRadius:'5px',marginTop:'20px'}}>
-                <div style={{display:'flex'}}>
-                  <div>
-                    <Avatar size='xs'/>
-                  </div>
-                  <div>
-                    <span style={{fontSize:'12px'}}> {user.displayName}</span>
-                  </div>
-                </div>
-             <p style={{width:'200px',margin:'5px'}} onClick={()=>deleteMessage(item.id)}>{item.text}</p> 
-          </WrapItem>
-      } 
-      </Wrap>
-      </div> 
-    </div>
-     ))
-
-     }
-  
-  
-
- 
-
-
-
-
-  </GridItem>
-  <GridItem  area={'footer'}>
-    <form onSubmit={handleSubmit} action="">
-
-          <Input h='30px' bg='#0092ca' style={{color:'white',borderRadius:'0px 0px 10px 10px'}} value={valueInput} onChange={handleChange } placeholder="message"></Input>
-
-    </form>
-  </GridItem>
-
- 
- 
-</Grid>
-  </Center> */}
  
     </div>
   );
